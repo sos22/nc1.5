4d6f54923cf93cf237ab3ffbbbd32f5d966bc22b
Steven Smith <sos22@cam.ac.uk>
2012-11-30 12:39:41 +0000

    netfront_info::rx_pfn_array is never used.  Remove it.

--- drivers/net/xen-netfront.c
+++ drivers/net/xen-netfront.c
@@ -125,7 +125,6 @@ struct netfront_info {
 	grant_ref_t gref_rx_head;
 	grant_ref_t grant_rx_ref[NET_RX_RING_SIZE];
 
-	unsigned long rx_pfn_array[NET_RX_RING_SIZE];
 	struct multicall_entry rx_mcl[NET_RX_RING_SIZE+1];
 	struct mmu_update rx_mmu[NET_RX_RING_SIZE];
 
